ccd2dba62454a2e2a0469a88fc879b1e69edda20
zidol <ajh3166@naver.com>
2023-06-29 00:05:31 +0900

    외부설정 사용 - @Value

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,11 +1,13 @@
 package hello;
 
 import hello.config.MyDataSourceEnvConfig;
+import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceEnvConfig.class)
+@Import(MyDataSourceValueConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")//config 패키지에 예제 코드가 계속 바뀌어서 @Import로 수동으로 등록 중 
 public class ExternalReadApplication {
 
