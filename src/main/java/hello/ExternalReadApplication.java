5e4320185917ca074f437408a4f77b2fd9510bed
zidol <ajh3166@naver.com>
2023-07-02 14:42:43 +0900

    외부설정 사용 - @ConfigurationProperties(1)

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,14 +1,19 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV1;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
+import hello.datasource.MyDataSourcePropertiesV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.datasource")//config 패키지에 예제 코드가 계속 바뀌어서 @Import로 수동으로 등록 중 
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
+@SpringBootApplication(scanBasePackages = "hello.datasource")//config 패키지에 예제 코드가 계속 바뀌어서 @Import로 수동으로 등록 중
+@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
