aa0ad02f681f541bc37bd7f1dde47480821b774d
zidol <ajh3166@naver.com>
2023-07-02 15:29:22 +0900

    @Profile example

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Import;
 //@Import(MyDataSourceConfigV1.class)
 //@Import(MyDataSourceConfigV2.class)
 @Import(MyDataSourceConfigV3.class)
-@SpringBootApplication(scanBasePackages = "hello.datasource")//config 패키지에 예제 코드가 계속 바뀌어서 @Import로 수동으로 등록 중
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})//config 패키지에 예제 코드가 계속 바뀌어서 @Import로 수동으로 등록 중
 @ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
